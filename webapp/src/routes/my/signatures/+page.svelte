b580e791d80f91e1e38cd4a279611d6ed9bd27f1
Puria Nafisi Azizi <puria@dyne.org>
2023-06-28 13:12:40 +0200

    ci: add missing deps

--- webapp/src/routes/my/signatures/+page.svelte
+++ webapp/src/routes/my/signatures/+page.svelte
@@ -1,33 +1,47 @@
 <script lang="ts">
-	import { Collections } from '$lib/pocketbase-types';
-	import CrudTable from '$lib/schema/CRUDTable.svelte';
-
-	export let data;
 	import Chip from '$lib/components/table/cells/chip.svelte';
 	import File from '$lib/components/table/cells/file.svelte';
+	import { currentUser } from '$lib/pocketbase';
+	import { Collections, type CrudExampleRecord } from '$lib/pocketbase-types';
+	import RecordsManager, {
+		createSlotTypeCaster
+	} from '$lib/schema/recordsManager/recordsManager.svelte';
+	import RecordsManagerTopbar from '$lib/schema/recordsManager/recordsManagerTopbar.svelte';
+	import RecordsTable from '$lib/schema/recordsManager/views/recordsTable.svelte';
+
+	const slotTypeCaster = createSlotTypeCaster<CrudExampleRecord>();
 </script>
 
 <div class="p-4">
-	<CrudTable
+	<RecordsManager
 		collection={Collections.Signatures}
-		fields={['type', 'title', 'file', 'folder', 'description']}
-		fieldsDisplay={{
-			type: Chip,
-			file: File
+		formSettings={{
+			hiddenFields: ['owner'],
+			hiddenFieldsValues: { owner: $currentUser?.id }
 		}}
-		actions={[
-			{
-				name: 'more',
-				function: (r) => {
-					console.log(r);
+		{slotTypeCaster}
+		let:records
+	>
+		<RecordsManagerTopbar />
+		<RecordsTable
+			{records}
+			fields={['type', 'title', 'file', 'folder', 'description']}
+			fieldsDisplay={{
+				type: Chip,
+				file: File
+			}}
+			actions={[
+				{
+					name: 'more',
+					function: (r) => {
+						console.log(r);
+					}
 				}
-			}
-		]}
-		relationsDisplayFields={{
-			folder: ['name'],
-			owner: ['email', 'username']
-		}}
-		formHiddenFields={['owner']}
-		formHiddenFieldsValues={{ owner: data.user?.id }}
-	/>
+			]}
+			relationsDisplayFields={{
+				folder: ['name'],
+				owner: ['email', 'username']
+			}}
+		/>
+	</RecordsManager>
 </div>
