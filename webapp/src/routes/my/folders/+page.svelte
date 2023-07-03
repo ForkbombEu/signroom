59123e21325817d57ae4ab6a08fcf35d0bb79ebd
Puria Nafisi Azizi <puria@dyne.org>
2023-07-03 18:36:03 +0200

    ui: cahnge folder from table to cards view

--- webapp/src/routes/my/folders/+page.svelte
+++ webapp/src/routes/my/folders/+page.svelte
@@ -5,7 +5,9 @@
 		createSlotTypeCaster
 	} from '$lib/schema/recordsManager/recordsManager.svelte';
 	import RecordsManagerTopbar from '$lib/schema/recordsManager/recordsManagerTopbar.svelte';
-	import RecordsTable from '$lib/schema/recordsManager/views/recordsTable.svelte';
+	import RecordCard from '$lib/schema/recordsManager/views/recordCard.svelte';
+	import { Heading } from 'flowbite-svelte';
+	import Chip from '$lib/schema/recordsManager/views/fieldsComponents/cells/chip.svelte';
 
 	const slotTypeCaster = createSlotTypeCaster<CrudExampleRecord>();
 </script>
@@ -21,6 +23,22 @@
 		let:records
 	>
 		<RecordsManagerTopbar />
-		<RecordsTable {records} fields={['id', 'name']} />
+		<div class="space-y-4">
+			<Heading tag="h4">Cards</Heading>
+			<div class="gap-4 grid grid-cols-2">
+				{#each records as record}
+					<div class="grow">
+						<RecordCard
+							{record}
+							titleField="name"
+							fields={['name']}
+							showEdit
+							showCheckbox
+							showDelete
+						/>
+					</div>
+				{/each}
+			</div>
+		</div>
 	</RecordsManager>
 </div>
