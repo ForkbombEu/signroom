515912ff0021cd78b6f309193814be59d409a6f2
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-07-11 19:37:04 +0200

    fix(admin:signatures): updated api rule

---
+++ admin/pb_migrations/1689096366_updated_signatures.js
@@ -0,0 +1,17 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("xl2wr8ij9rnkug6")
+
+  collection.listRule = "owner.id = @request.auth.id || (@collection.authorizations.users.id ?= @request.auth.id && @collection.authorizations.record_id = id)"
+  collection.viewRule = "owner.id = @request.auth.id || (@collection.authorizations.users.id ?= @request.auth.id && @collection.authorizations.record_id = id)"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("xl2wr8ij9rnkug6")
+
+  collection.listRule = null
+  collection.viewRule = null
+
+  return dao.saveCollection(collection)
+})
