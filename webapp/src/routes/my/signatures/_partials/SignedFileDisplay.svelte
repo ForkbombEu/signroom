5ba714914d264c0d1dfc2c243482388d509f6284
phoebus-84 <ennio@dyne.org>
2023-07-11 17:17:04 +0200

    feat: make signed file downloadable as json

--- webapp/src/routes/my/signatures/_partials/SignedFileDisplay.svelte
+++ webapp/src/routes/my/signatures/_partials/SignedFileDisplay.svelte
@@ -10,17 +10,32 @@
 	import type { Record } from 'pocketbase';
 	import type { SignaturesRecord } from '$lib/pocketbase-types';
 	import { Button, Modal } from 'flowbite-svelte';
-	import { DocumentCheck } from 'svelte-heros-v2';
+	import { DocumentArrowDown, DocumentCheck } from 'svelte-heros-v2';
 	import RenderSignedFile from './RenderSignedFile.svelte.svelte';
 
 	export let value: SignedFile;
 	export let record: Record & SignaturesRecord;
 
 	let open = false;
-    const {type} = record;
+	const { type } = record;
 
+	const onDownload = ()=> {
+		const a = document.createElement('a');
+		const file = new Blob([JSON.stringify({ type: record.type, signedFile: value })], { type: 'text/plain' });
+		a.href = URL.createObjectURL(file);
+		a.download = `${record.title}.json`
+		a.click();
+	}
 </script>
 
+<Button
+	target="_blank"
+	class="!p-2"
+	color="alternative"
+	on:click={onDownload}>
+	 <DocumentArrowDown size="20" />
+	</Button
+>
 <Button target="_blank" class="!p-2" color="alternative" on:click={() => (open = !open)}>
 	<DocumentCheck size="20" />
 </Button>
