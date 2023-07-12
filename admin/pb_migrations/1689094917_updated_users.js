aa4de3dbc32b61faa04ccd402fd9f448500f4782
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-07-12 11:07:12 +0200

    feat: share signature (#42)
    * chore: cruft.json update
    
    * chore: removed old migrations
    
    * chore: fixed cruft.json
    
    * chore: update cruft.json
    
    * chore: cruft update
    
    * chore: generate types
    
    * feat(admin): signatures: add api rule
    
    * feat(admin): users: made email public
    
    * feat: add share signature component
    
    * chore: cruft update
    
    * fix(admin:signatures): updated api rule
    
    * fix(admin:authorizations): updated api rule
    
    * chore: schema update
    
    * feat(signatures): updated share modal
    
    * chore: cruft update
    
    * chore: cruft update
    
    * feat(signatures): add authorization remove
    
    * feat(signature): add toast notification

---
+++ admin/pb_migrations/1689094917_updated_users.js
@@ -0,0 +1,17 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  collection.listRule = "@request.auth.id != ''"
+  collection.viewRule = "@request.auth.id != ''"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  collection.listRule = null
+  collection.viewRule = null
+
+  return dao.saveCollection(collection)
+})
--- webapp/src/routes/my/signatures/+page.svelte
+++ webapp/src/routes/my/signatures/+page.svelte
@@ -5,16 +5,22 @@
 	import File from '$lib/schema/recordsManager/views/fieldsComponents/cells/file.svelte';
 
 	import { currentUser } from '$lib/pocketbase';
-	import { Collections, type CrudExampleRecord } from '$lib/pocketbase-types';
+	import { Collections, type SignaturesRecord } from '$lib/pocketbase-types';
 	import RecordsManager, {
 		createSlotTypeCaster
 	} from '$lib/schema/recordsManager/recordsManager.svelte';
 	import RecordsTable from '$lib/schema/recordsManager/views/recordsTable.svelte';
 	import { page } from '$app/stores';
-	import type { RecordFullListQueryParams } from 'pocketbase';
+	import type { Record, RecordFullListQueryParams } from 'pocketbase';
 	import SignaturesFoldersHead from '$lib/components/signaturesFoldersHead.svelte';
 	import SignedFileDisplay from './_partials/SignedFileDisplay.svelte';
-	const slotTypeCaster = createSlotTypeCaster<CrudExampleRecord>();
+	import { Button, Toast } from 'flowbite-svelte';
+	import { Share } from 'svelte-heros-v2';
+	import ShareSignature from './_partials/ShareSignature.svelte';
+	import OwnerDisplay from './_partials/OwnerDisplay.svelte';
+	import { slide } from 'svelte/transition';
+
+	const slotTypeCaster = createSlotTypeCaster<SignaturesRecord>();
 
 	$: folderId = $page.url.searchParams.get('folder');
 
@@ -23,6 +29,40 @@
 		initialQueryParams = { filter: `folder.id="${folderId}"` };
 	} else {
 		initialQueryParams = {};
+	}
+
+	let shareModal = false;
+	let record: (Record & SignaturesRecord) | undefined = undefined;
+
+	function openShareModal(r: SignaturesRecord & Record) {
+		shareModal = true;
+		record = r;
+	}
+
+	function clearRecord() {
+		record = undefined;
+	}
+
+	/* Toasts */
+
+	const toasts = {
+		add: '✅ Signature shared successfully',
+		remove: '✅ Signature unshared successfully'
+	};
+
+	type Toast = keyof typeof toasts;
+
+	let show = false;
+	let content: string | undefined = undefined;
+	const duration = 2000;
+
+	function trigger(key: Toast) {
+		show = true;
+		content = toasts[key];
+		setTimeout(() => {
+			show = false;
+			content = undefined;
+		}, duration);
 	}
 </script>
 
@@ -46,18 +86,51 @@
 			{:else}
 				<SignaturesFoldersHead {folderId} />
 			{/if}
-      <RecordsTable
-        {records}
-        fields={['type', 'title', 'file', 'signed_file','description']}
-        showCheckboxes={false}
-        fieldsComponents={{
-          type: Chip,
-          file: File,
-          description: Description,
-          //@ts-ignore
-          signed_file:SignedFileDisplay
-        }}
-      />
+			<RecordsTable
+				{records}
+				fields={['type', 'owner', 'title', 'file', 'signed_file', 'description']}
+				showCheckboxes={false}
+				fieldsComponents={{
+					type: Chip,
+					file: File,
+					description: Description,
+					owner: OwnerDisplay,
+					//@ts-ignore
+					signed_file: SignedFileDisplay
+				}}
+				let:record
+			>
+				{#if record.owner == $currentUser?.id}
+					<Button
+						class="!p-2"
+						color="alternative"
+						on:click={() => {
+							openShareModal(record);
+						}}
+					>
+						<Share size="20" />
+					</Button>
+				{/if}
+			</RecordsTable>
 		</RecordsManager>
 	{/key}
 </div>
+
+{#key record}
+	{#if record}
+		<ShareSignature
+			bind:open={shareModal}
+			{record}
+			on:add={() => {
+				trigger('add');
+			}}
+			on:remove={() => {
+				trigger('remove');
+			}}
+		/>
+	{/if}
+{/key}
+
+<Toast simple position="bottom-right" color="dark" transition={slide} bind:open={show}>
+	{content}
+</Toast>
