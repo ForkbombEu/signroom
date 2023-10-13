c0520283cb03e9216d43181946e94404bd2d1c8e
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-10-13 14:45:07 +0200

    test(saas): add JSONSchemaBuilder component

---
+++ webapp/src/routes/tests/create-template/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { JSONSchemaBuilder, type JSONSchema } from 'json-schema-builder-svelte';
+
+	let result: JSONSchema | undefined;
+</script>
+
+<JSONSchemaBuilder
+	on:create={(e) => {
+		result = e.detail.schema;
+	}}
+></JSONSchemaBuilder>
+
+{#if result}
+	<pre>{JSON.stringify(result, null, 2)}</pre>
+{/if}
