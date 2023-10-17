a54e0e79f34b29bef8221e6665f425d00cc20d9c
phoebus-84 <ennio@dyne.org>
2023-10-17 08:25:12 +0200

    feat: templates authorization and crud manager

---
+++ webapp/src/lib/components/addTemplate.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" context="module">
+	const toasts = {
+		add: '✅ Signature shared successfully',
+		remove: '✅ Signature unshared successfully',
+		signed: '✅ Document signed successfully'
+	};
+	export type ToastContent = keyof typeof toasts;
+</script>
+
+<script lang="ts">
+	import { Collections } from '$lib/pocketbase/types';
+	import { CollectionManager, CollectionTable, EditRecord } from '$lib/collectionManager';
+</script>
+
+<CollectionManager collection={Collections.Templates} let:records>
+	<CollectionTable {records}></CollectionTable>
+</CollectionManager>
