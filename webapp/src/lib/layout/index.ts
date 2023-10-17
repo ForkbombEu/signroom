bc4ce75107e5fb85aa84ec793c6c65207741efa4
matteo-cristino <cristino.matteo97@gmail.com>
2023-10-17 15:05:15 +0200

    fix(webapp): consistent file name case

--- webapp/src/lib/layout/index.ts
+++ webapp/src/lib/layout/index.ts
@@ -1,4 +1,4 @@
-import UIShell from './UiShell.svelte';
+import UIShell from './UIShell.svelte';
 import type { SidebarLink } from './SidebarLinks.svelte';
 import AvatarMenu from './AvatarMenu.svelte';
 import HamburgerButton from './HamburgerButton.svelte';
