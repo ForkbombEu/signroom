3107ebf4e9257a19520e0bb0788f4abe43797ea9
phoebus-84 <ennio@dyne.org>
2023-10-23 13:44:36 +0200

    feat: add breadcrumbs navigation

---
+++ webapp/src/routes/my/organizations/[id]/(settings)/+layout.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { pb } from '$lib/pocketbase';
+	import { ProtectedOrgLayout, ProtectedOrgUI, OrgRoles } from '$lib/rbac';
+	import {
+		A,
+		Button,
+		ButtonGroup,
+		Heading,
+		Hr,
+		P
+	} from 'flowbite-svelte';
+
+	export let data;
+	$: organization = data.organization;
+	$: avatarPath = pb.files.getUrl(organization, organization.avatar);
+
+	const href = (path = '') => `/my/organizations/${organization.id}${path}`;
+	const { OWNER, ADMIN } = OrgRoles;
+</script>
+
+<!--  -->
+
+<div class="flex justify-between items-center">
+	<Heading tag="h3"><A href={href()}>{organization.name}</A></Heading>
+	<ProtectedOrgUI orgId={organization.id} roles={[OWNER, ADMIN]}>
+		<div class="flex justify-end items-center space-x-4">
+			<P color="text-gray-600" size="sm">Settings</P>
+			<ButtonGroup>
+				<ProtectedOrgUI orgId={organization.id} roles={[OWNER]}>
+					<Button href={href('/general')}>General</Button>
+				</ProtectedOrgUI>
+				<Button href={href('/members')}>Members</Button>
+			</ButtonGroup>
+		</div>
+	</ProtectedOrgUI>
+</div>
+
+<Hr />
+
+<ProtectedOrgLayout orgId={organization.id}>
+	<slot />
+</ProtectedOrgLayout>
--- webapp/src/routes/my/organizations/[id]/+layout.svelte
+++ webapp/src/routes/my/organizations/[id]/+layout.svelte
@@ -1,35 +1,42 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { pb } from '$lib/pocketbase';
-	import { ProtectedOrgLayout, ProtectedOrgUI, OrgRoles } from '$lib/rbac';
-	import { A, Button, ButtonGroup, Heading, Hr, P } from 'flowbite-svelte';
+	import { ProtectedOrgLayout } from '$lib/rbac';
+	import { Breadcrumb, BreadcrumbItem, Hr } from 'flowbite-svelte';
+	import { UserGroup } from 'svelte-heros-v2';
 
 	export let data;
 	$: organization = data.organization;
 	$: avatarPath = pb.files.getUrl(organization, organization.avatar);
+	$: activeUrl = $page.url.pathname;
 
-	const href = (path = '') => `/my/organizations/${organization.id}${path}`;
-	const { OWNER, ADMIN } = OrgRoles;
+	const href = (path = '', i = 0) => {
+		let p = '';
+		Array.from(Array(i).keys()).forEach((x) => {
+			p = `${p}/${pathFragments[x]}`;
+			console.log(p, i);
+		});
+		return `/my/organizations/${organization.id}${p}${path}`;
+	};
+	$: pathFragments = (activeUrl && activeUrl.split(`${organization.id}/`)[1]?.split('/')) || [];
 </script>
 
 <!--  -->
 
-<A href="/my/organizations" class="mb-4">← My organizations</A>
-
-<div class="flex justify-between items-center">
-	<Heading tag="h3"><A href={href()}>{organization.name}</A></Heading>
-	<ProtectedOrgUI orgId={organization.id} roles={[OWNER, ADMIN]}>
-		<div class="flex justify-end items-center space-x-4">
-			<P color="text-gray-600" size="sm">Settings</P>
-			<ButtonGroup>
-				<ProtectedOrgUI orgId={organization.id} roles={[OWNER]}>
-					<Button href={href('/general')}>General</Button>
-				</ProtectedOrgUI>
-				<Button href={href('/members')}>Members</Button>
-			</ButtonGroup>
-		</div>
-	</ProtectedOrgUI>
-</div>
-
+<ProtectedOrgLayout orgId={organization.id}>
+	<Breadcrumb aria-label="breadcrumb">
+		<BreadcrumbItem href="/my/organizations" home>
+			<svelte:fragment slot="icon">
+				<UserGroup class="w-5 h-5 mr-2" />
+			</svelte:fragment>
+			Organizations</BreadcrumbItem
+		>
+		<BreadcrumbItem href={href()}>{organization.name}</BreadcrumbItem>
+		{#each pathFragments as link, i}
+			<BreadcrumbItem href={href(`/${link}`, i)}>{link}</BreadcrumbItem>
+		{/each}
+	</Breadcrumb>
+</ProtectedOrgLayout>
 <Hr />
 
 <ProtectedOrgLayout orgId={organization.id}>
