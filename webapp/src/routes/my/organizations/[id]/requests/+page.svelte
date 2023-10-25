e406301adda392e178feef121911ed60b4ab5c9a
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-10-25 15:46:03 +0200

    feat: join requests page for admins

---
+++ webapp/src/routes/my/organizations/[id]/requests/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import CollectionManager from '$lib/collectionManager/collectionManager.svelte';
+	import CollectionTable from '$lib/collectionManager/ui/collectionTable.svelte';
+	import { Collections } from '$lib/pocketbase/types';
+
+	export let data;
+	$: organization = data.organization;
+</script>
+
+<CollectionManager
+	collection={Collections.OrgJoinRequests}
+	initialQueryParams={{
+		filter: `organization.id = "${organization.id}"`
+	}}
+	let:records
+>
+	<CollectionTable {records}></CollectionTable>
+</CollectionManager>
