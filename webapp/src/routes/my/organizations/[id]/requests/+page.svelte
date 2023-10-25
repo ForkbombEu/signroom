72b687c51a065542440bc7498b149e9389968754
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-10-25 16:05:17 +0200

    feat: admin join requests page: improve records manager

--- webapp/src/routes/my/organizations/[id]/requests/+page.svelte
+++ webapp/src/routes/my/organizations/[id]/requests/+page.svelte
@@ -1,18 +1,47 @@
-<script>
+<script lang="ts">
+	import { CollectionEmptyState } from '$lib/collectionManager';
 	import CollectionManager from '$lib/collectionManager/collectionManager.svelte';
+	import CollectionManagerHeader from '$lib/collectionManager/ui/collectionManagerHeader.svelte';
 	import CollectionTable from '$lib/collectionManager/ui/collectionTable.svelte';
-	import { Collections } from '$lib/pocketbase/types';
+	import {
+		Collections,
+		type OrgJoinRequestsRecord,
+		type UsersResponse
+	} from '$lib/pocketbase/types';
+	import { createTypeProp } from '$lib/utils/typeProp.js';
+	import { Heading } from 'flowbite-svelte';
+	import { UserGroup } from 'svelte-heros-v2';
 
 	export let data;
 	$: organization = data.organization;
+
+	const recordType = createTypeProp<OrgJoinRequestsRecord>();
+	const expandType = createTypeProp<{ user: UsersResponse }>();
 </script>
 
 <CollectionManager
 	collection={Collections.OrgJoinRequests}
 	initialQueryParams={{
-		filter: `organization.id = "${organization.id}"`
+		filter: `organization.id = "${organization.id}"`,
+		expand: 'user'
 	}}
+	{recordType}
+	{expandType}
 	let:records
 >
-	<CollectionTable {records}></CollectionTable>
+	<CollectionManagerHeader>
+		<svelte:fragment slot="title">
+			<Heading tag="h5">Manage join requests</Heading>
+		</svelte:fragment>
+	</CollectionManagerHeader>
+	<CollectionTable {records} fields={['user', 'email']} hideActions={['edit', 'share']}>
+		<svelte:fragment slot="emptyState">
+			<CollectionEmptyState
+				hideCreateButton
+				description=""
+				title="No join requests"
+				icon={UserGroup}
+			/>
+		</svelte:fragment>
+	</CollectionTable>
 </CollectionManager>
