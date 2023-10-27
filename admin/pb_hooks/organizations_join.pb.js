cc8a0514b32d40d4889afb2520d4fc15eb2fbb2e
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-10-27 15:16:11 +0200

    feat: add email notifications for `orgJoinRequests` (#281)
    * fix: critical: role was not created
    
    * feat: notify admins and owners on join request create
    
    * feat: add join request user notification

--- admin/pb_hooks/organizations_join.pb.js
+++ admin/pb_hooks/organizations_join.pb.js
@@ -5,3 +5,97 @@ onRecordBeforeCreateRequest((e) => {
     e.record.set("status", "pending");
     e.record.set("reminders", 0);
 }, "orgJoinRequests");
+
+onRecordAfterCreateRequest((e) => {
+    try {
+        console.log("Hook - orgJoinRequests - Sending email to admins");
+
+        $app.dao().expandRecord(e.record, ["organization"]);
+        const organization = e.record.expandedOne("organization");
+        const organizationId = organization.getId();
+        const organizationName = organization.get("name");
+
+        const recipients = $app
+            .dao()
+            .findRecordsByFilter(
+                "orgAuthorizations",
+                `organization.id = "${organizationId}" && ( role.name = "admin" || role.name = "owner" )`
+            );
+        $app.dao().expandRecords(recipients, ["user"]);
+
+        /**
+         * @type {mail.Address[]}
+         */
+        const recipientsAddresses = recipients
+            .map((r) => r.expandedOne("user").get("email"))
+            .map((e) => ({ address: e }));
+
+        const message = new MailerMessage({
+            from: {
+                address: $app.settings().meta.senderAddress,
+                name: $app.settings().meta.senderName,
+            },
+            to: recipientsAddresses,
+            subject: `${organizationName} | New join request`,
+            html: "Your organization receved a new join request",
+        });
+
+        $app.newMailClient().send(message);
+    } catch (e) {
+        console.log(e);
+        console.log(JSON.stringify(e));
+    }
+}, "orgJoinRequests");
+
+onRecordAfterUpdateRequest((e) => {
+    try {
+        console.log("Hook - orgJoinRequests - Sending email to user");
+
+        const USER_KEY = "user";
+        const ORGANIZATION_KEY = "organization";
+
+        const status = e.record.get("status");
+        if (status == "pending") return;
+
+        $app.dao().expandRecord(e.record, [USER_KEY, ORGANIZATION_KEY]);
+
+        const userEmail = e.record.expandedOne(USER_KEY).get("email");
+        /** @type {mail.Address} */
+        const userAddress = { address: userEmail };
+
+        const organizationName = e.record
+            .expandedOne(ORGANIZATION_KEY)
+            .get("name");
+
+        /**
+         * @typedef {Object} EmailContent
+         * @property {string} subject
+         * @property {string} html
+         */
+
+        /** @type {EmailContent} */
+        const successEmail = {
+            subject: `${organizationName} | Request accepted`,
+            html: `Welcome to ${organizationName}! Your request has been accepted!`,
+        };
+        /** @type {EmailContent} */
+        const rejectEmail = {
+            subject: `${organizationName} | Request declined`,
+            html: "Your join request has been declined.",
+        };
+
+        const message = new MailerMessage({
+            from: {
+                address: $app.settings().meta.senderAddress,
+                name: $app.settings().meta.senderName,
+            },
+            to: [userAddress],
+            ...(status == "accepted" ? successEmail : rejectEmail),
+        });
+
+        $app.newMailClient().send(message);
+    } catch (e) {
+        console.log(e);
+        console.log(JSON.stringify(e));
+    }
+}, "orgJoinRequests");
