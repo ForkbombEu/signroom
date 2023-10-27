049ef893f6a77cbd3bd35899877c3ee4f6d4ff77
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-10-27 12:20:32 +0200

    feat: notify admins and owners on join request create

--- admin/pb_hooks/organizations_join.pb.js
+++ admin/pb_hooks/organizations_join.pb.js
@@ -5,3 +5,44 @@ onRecordBeforeCreateRequest((e) => {
     e.record.set("status", "pending");
     e.record.set("reminders", 0);
 }, "orgJoinRequests");
+
+onRecordAfterCreateRequest((e) => {
+    try {
+        console.log("Hook - orgJoinRequests - Sending email to admins");
+
+        $app.dao().expandRecord(e.record, ["organization"]);
+        const organization = e.record.expandedOne("organization");
+        const organizationId = organization.getId();
+        const organizationName = organization.get("name");
+
+        const recipients = $app
+            .dao()
+            .findRecordsByFilter(
+                "orgAuthorizations",
+                `organization.id = "${organizationId}" && ( role.name = "admin" || role.name = "owner" )`
+            );
+        $app.dao().expandRecords(recipients, ["user"]);
+
+        /**
+         * @type {mail.Address[]}
+         */
+        const recipientsAddresses = recipients
+            .map((r) => r.expandedOne("user").get("email"))
+            .map((e) => ({ address: e }));
+
+        const message = new MailerMessage({
+            from: {
+                address: $app.settings().meta.senderAddress,
+                name: $app.settings().meta.senderName,
+            },
+            to: recipientsAddresses,
+            subject: `${organizationName} | New join request`,
+            html: "Your organization receved a new join request",
+        });
+
+        $app.newMailClient().send(message);
+    } catch (e) {
+        console.log(e);
+        console.log(JSON.stringify(e));
+    }
+}, "orgJoinRequests");
