9df2e02f7935d53d50345d16be20ab6d89a6730b
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-10-27 10:09:34 +0200

    chore: renamed hook

--- admin/pb_hooks/organizations_join.pb.js
+++ admin/pb_hooks/organizations_join.pb.js
@@ -3,4 +3,5 @@
 onRecordBeforeCreateRequest((e) => {
     console.log("Hook - orgJoinRequests - Setting pending status");
     e.record.set("status", "pending");
+    e.record.set("reminders", 0);
 }, "orgJoinRequests");
