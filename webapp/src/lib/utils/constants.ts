4671bbc4b780d1aecdb56b369ea90a583dd24a87
puria <puria@users.noreply.github.com>
2023-11-21 02:28:01 +0000

    chore: accept new Cruft update

---
+++ webapp/src/lib/utils/constants.ts
@@ -0,0 +1,5 @@
+export const welcomeSearchParamKey = 'welcome';
+export const welcomeSearchParam = `${welcomeSearchParamKey}=true`;
+
+export const missingKeyringParamKey = 'missing_keyring';
+export const missingKeyringParam = `${missingKeyringParamKey}=true`;
