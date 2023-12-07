df4860309e4a07ef7af5d40f8892b4ea67035858
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-12-07 16:16:43 +0100

    feat(service): add "download issuer" button

--- webapp/src/routes/my/organizations/[id]/services/[serviceId]/+page.svelte
+++ webapp/src/routes/my/organizations/[id]/services/[serviceId]/+page.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
+	import { Button, Hr } from 'flowbite-svelte';
+	import { ArrowDownTray } from 'svelte-heros-v2';
 	import ServiceForm from '../_partials/serviceForm.svelte';
 
 	export let data;
 </script>
 
-<pre>{JSON.stringify(data, null, 2)}</pre>
-<ServiceForm organizationId={data.organization.id} initialData={data.service} mode="edit" />
+<div class="space-y-8">
+	<div class="flex justify-end">
+		<Button color="alternative">
+			<ArrowDownTray></ArrowDownTray>
+			<span class="ml-2"> Download credential issuer </span>
+		</Button>
+	</div>
+	<Hr />
+	<div class="max-h-96 overflow-scroll border rounded-lg p-4">
+		<pre>{JSON.stringify(data, null, 2)}</pre>
+	</div>
+	<Hr />
+	<ServiceForm organizationId={data.organization.id} initialData={data.service} mode="edit" />
+</div>
