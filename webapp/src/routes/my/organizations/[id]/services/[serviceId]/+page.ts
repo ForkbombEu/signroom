22e73bc29f0f1aaa13cd6a28b813df6479ba4120
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-12-11 17:20:45 +0100

    fix: correct types in service fetch

--- webapp/src/routes/my/organizations/[id]/services/[serviceId]/+page.ts
+++ webapp/src/routes/my/organizations/[id]/services/[serviceId]/+page.ts
@@ -1,10 +1,17 @@
 import { pb } from '$lib/pocketbase';
-import { Collections, type ServicesResponse } from '$lib/pocketbase/types.js';
+import {
+	Collections,
+	type ServicesResponse,
+	type TemplatesResponse
+} from '$lib/pocketbase/types.js';
 
 export const load = async ({ params }) => {
 	const { serviceId } = params;
+
+	const expand = 'templates';
+
 	const service = await pb
 		.collection(Collections.Services)
-		.getOne<ServicesResponse>(serviceId, { expand: 'templates' });
+		.getOne<ServicesResponse<{ [expand]: TemplatesResponse[] }>>(serviceId, { expand });
 	return { service };
 };
