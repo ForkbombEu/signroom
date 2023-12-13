6d1421e53706bbb0228298cfa4ba1979ab4e60c4
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-12-13 11:29:55 +0100

    feat(credentialIssuer): created template function

--- webapp/src/lib/credentialIssuer/index.ts
+++ webapp/src/lib/credentialIssuer/index.ts
@@ -1,11 +1,66 @@
 import type { ObjectSchema } from '$lib/jsonSchema/types';
-import type { TemplatesResponse } from '$lib/pocketbase/types';
-import type { CredentialMetadata, CredentialSubject, CredentialSubjectItem } from './types';
+import type { CredentialSubject, CredentialSubjectItem, CredentialIssuerMetadata } from './types';
 
-export function objectSchemaToCredentialSubject(
-	schema: ObjectSchema,
-	type: string[] = []
-): CredentialSubject {
+//
+
+type CredentialIssuerMetadataTemplateProps = {
+	credential_issuer_url: string;
+	credential_issuer_name: string;
+	credential_name: string;
+	authorization_server: string;
+	credentialSubject: CredentialSubject;
+};
+
+export function credentialIssuerMetadataTemplate({
+	credential_issuer_url,
+	credential_issuer_name,
+	credential_name,
+	authorization_server,
+	credentialSubject
+}: CredentialIssuerMetadataTemplateProps): CredentialIssuerMetadata {
+	return {
+		credential_issuer: credential_issuer_url,
+		authorization_servers: [authorization_server],
+		credential_endpoint: `${credential_issuer_url}`, // TODO - Review
+		batch_credential_endpoint: `${credential_issuer_url}/batch_credential`, // TODO - Review
+		deferred_credential_endpoint: `${credential_issuer_url}/deferred_credential`, // TODO - Review
+		display: [
+			{
+				name: credential_issuer_name,
+				locale: 'en-US'
+			}
+		],
+		credentials_supported: {
+			[credential_name]: {
+				format: 'jwt_vc_json',
+				// scope: 'UniversityDegree', // not sure
+				cryptographic_binding_methods_supported: ['did:dyne:sandbox.signroom'],
+				cryptographic_suites_supported: ['ES256K'],
+				proof_types_supported: ['jwt'],
+				credential_definition: {
+					type: [credential_name],
+					credentialSubject
+				}
+				// display: [
+				// 	{
+				// 		name: 'University Credential',
+				// 		locale: 'en-US',
+				// 		logo: {
+				// 			url: 'https://exampleuniversity.com/public/logo.png',
+				// 			alt_text: 'a square logo of a university'
+				// 		},
+				// 		background_color: '#12107c',
+				// 		text_color: '#FFFFFF'
+				// 	}
+				// ]
+			}
+		}
+	};
+}
+
+//
+
+export function objectSchemaToCredentialSubject(schema: ObjectSchema): CredentialSubject {
 	const credentialSubject: CredentialSubject = {};
 	for (const [propertyName, property] of Object.entries(schema.properties)) {
 		if (property.type != 'object' && property.type != 'array') {
@@ -13,20 +68,11 @@ export function objectSchemaToCredentialSubject(
 				mandatory: Boolean(schema.required?.includes(propertyName))
 			} satisfies CredentialSubjectItem;
 		} else if (property.type === 'object') {
-			credentialSubject[propertyName] = objectSchemaToCredentialSubject(property, type);
+			credentialSubject[propertyName] = objectSchemaToCredentialSubject(property);
 		} else {
-			console.log(`Property not handled: ${propertyName}`);
+			console.log(`Property not handled:`);
+			console.log(JSON.stringify(property, null, 2));
 		}
 	}
 	return credentialSubject;
-}
-
-export function templateToCredentialMetadata(template: TemplatesResponse): CredentialMetadata {
-	return {
-		format: 'jwt_vc_json',
-		credential_definition: {
-			type: [],
-			credentialSubject: objectSchemaToCredentialSubject(JSON.parse(template.schema))
-		}
-	};
 }
--- webapp/src/routes/api/downloadCredentialIssuer/index.ts
+++ webapp/src/routes/api/downloadCredentialIssuer/index.ts
@@ -1,18 +1,17 @@
-import type { CredentialIssuerMetadata, CredentialSubject } from '$lib/credentialIssuer/types';
 import { z } from 'zod';
 
-export const requestFormSchema = z.object({
-	credential_issuer: z.string().url(),
+export const formSchema = z.object({
+	credential_issuer_url: z.string().url(),
 	authorization_server: z.string().url()
 });
 
-export const requestBodySchema = requestFormSchema.extend({
+export const requestSchema = formSchema.extend({
 	credential_name: z.string(), // Generate from service name
-	templates: z.array(z.string()), // List of JSON schemas
-	credential_issuer_name: z.string() // Generate from organization name
+	credential_issuer_name: z.string(), // Generate from organization name
+	templates: z.array(z.string()) // List of JSON schemas
 });
 
-export type RequestBody = z.infer<typeof requestBodySchema>;
+export type RequestBody = z.infer<typeof requestSchema>;
 
 export function request(body: RequestBody, fetchFn = fetch) {
 	return fetchFn('/api/downloadCredentialIssuer', {
@@ -22,61 +21,4 @@ export function request(body: RequestBody, fetchFn = fetch) {
 			'content-type': 'application/json'
 		}
 	});
-}
-
-//
-
-type CredentialIssuerMetadataTemplateProps = {
-	credential_issuer_url: string;
-	credential_issuer_name: string;
-	authorization_server: string;
-	credential_name: string;
-	credentialSubject: CredentialSubject;
-};
-
-export function credentialIssuerMetadataTemplate({
-	credential_issuer_url,
-	credential_issuer_name,
-	authorization_server,
-	credential_name,
-	credentialSubject
-}: CredentialIssuerMetadataTemplateProps): CredentialIssuerMetadata {
-	return {
-		credential_issuer: credential_issuer_url,
-		authorization_servers: [authorization_server],
-		credential_endpoint: `${credential_issuer_url}`, // TODO - Review
-		batch_credential_endpoint: `${credential_issuer_url}/batch_credential`, // TODO - Review
-		deferred_credential_endpoint: `${credential_issuer_url}/deferred_credential`, // TODO - Review
-		display: [
-			{
-				name: credential_issuer_name,
-				locale: 'en-US'
-			}
-		],
-		credentials_supported: {
-			[credential_name]: {
-				format: 'jwt_vc_json',
-				// scope: 'UniversityDegree', // not sure
-				cryptographic_binding_methods_supported: ['did:dyne:sandbox.signroom'],
-				cryptographic_suites_supported: ['ES256K'],
-				proof_types_supported: ['jwt'],
-				credential_definition: {
-					type: [credential_name],
-					credentialSubject
-				}
-				// display: [
-				// 	{
-				// 		name: 'University Credential',
-				// 		locale: 'en-US',
-				// 		logo: {
-				// 			url: 'https://exampleuniversity.com/public/logo.png',
-				// 			alt_text: 'a square logo of a university'
-				// 		},
-				// 		background_color: '#12107c',
-				// 		text_color: '#FFFFFF'
-				// 	}
-				// ]
-			}
-		}
-	};
 }
