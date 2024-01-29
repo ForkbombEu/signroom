b2e1383823f8c01ff9148a3eee36982eb5b9b488
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-29 12:16:34 +0100

    feat(downloadMicroservice): edit create.schema.json

--- webapp/src/routes/api/downloadCredentialIssuer/+server.ts
+++ webapp/src/routes/api/downloadCredentialIssuer/+server.ts
@@ -5,6 +5,8 @@ import path from 'node:path';
 import * as credentialIssuer from '$lib/credentialIssuer';
 import * as credentialKeys from './credential.keys';
 import { merge } from 'lodash';
+import type { ObjectSchema } from '$lib/jsonSchema/types';
+import { nanoid } from 'nanoid';
 
 //
 
@@ -78,7 +80,17 @@ export const POST: RequestHandler = async ({ fetch, request }) => {
 			editFile(zip, CREDENTIAL_KEYS_FILE_NAME, JSON.stringify(template, null, 4));
 		}
 
-		//
+		/* create.schema.json */
+
+		const CREATE_SCHEMA_JSON_FILE_NAME = 'create.schema.json';
+		const createSchemaJsonEntry = getFile(zip, CREATE_SCHEMA_JSON_FILE_NAME);
+
+		if (createSchemaJsonEntry) {
+			const schema = mergeObjectSchemas(templates.map((t) => JSON.parse(t)));
+			editFile(zip, CREATE_SCHEMA_JSON_FILE_NAME, JSON.stringify(schema, null, 2));
+		}
+
+		/* */
 
 		return new Response(zip.toBuffer(), {
 			status: 200,
@@ -102,4 +114,16 @@ function editFile(zip: AdmZip, entryName: string, content: string) {
 	const entry = zip.getEntries().find((entry) => entry.name === entryName);
 	if (!entry) return;
 	zip.updateFile(entry, Buffer.from(content));
+}
+
+function mergeObjectSchemas(schemas: ObjectSchema[]): ObjectSchema {
+	if (schemas.length === 1) return schemas[0];
+
+	const mergedSchema: ObjectSchema = { type: 'object', properties: {}, required: [] };
+	for (const schema of schemas) {
+		const id = nanoid(5);
+		mergedSchema.properties[id] = schema;
+		mergedSchema.required?.push(id);
+	}
+	return mergedSchema;
 }
