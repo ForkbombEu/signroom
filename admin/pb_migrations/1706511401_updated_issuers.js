f5147c5c8291d4deada97f099ad7c861e6d6fba1
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-29 08:10:40 +0100

    feat: add "endpoint" field to issuers

---
+++ admin/pb_migrations/1706511401_updated_issuers.js
@@ -0,0 +1,45 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("ffk93r2b2r1fqjo")
+
+  collection.listRule = ""
+  collection.viewRule = ""
+  collection.createRule = "@collection.orgAuthorizations.user.id ?= @request.auth.id &&\n@collection.orgAuthorizations.organization.id ?= organization.id &&\n(@collection.orgAuthorizations.role.name ?= \"admin\" || \n @collection.orgAuthorizations.role.name ?= \"owner\")"
+  collection.updateRule = "@collection.orgAuthorizations.user.id ?= @request.auth.id &&\n@collection.orgAuthorizations.organization.id ?= organization.id &&\n(@collection.orgAuthorizations.role.name ?= \"admin\" || \n @collection.orgAuthorizations.role.name ?= \"owner\")"
+  collection.deleteRule = "@collection.orgAuthorizations.user.id ?= @request.auth.id &&\n@collection.orgAuthorizations.organization.id ?= organization.id &&\n(@collection.orgAuthorizations.role.name ?= \"admin\" || \n @collection.orgAuthorizations.role.name ?= \"owner\")"
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "zyyuw9kx",
+    "name": "organization",
+    "type": "relation",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "aako88kt3br4npt",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("ffk93r2b2r1fqjo")
+
+  collection.listRule = "@request.auth.id != ''"
+  collection.viewRule = "@request.auth.id != ''"
+  collection.createRule = "@request.auth.id != ''"
+  collection.updateRule = null
+  collection.deleteRule = null
+
+  // remove
+  collection.schema.removeField("zyyuw9kx")
+
+  return dao.saveCollection(collection)
+})
