238e0e88abdff2539218672d4fb9c3eb4c71bcb8
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-31 12:58:05 +0100

    feat: add language switch button

---
+++ webapp/src/lib/components/languageSwitcher.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import {
+		setLanguageTag,
+		availableLanguageTags,
+		type AvailableLanguageTag,
+		languageTag
+	} from '$paraglide/runtime';
+	import { Button, Dropdown, DropdownItem } from 'flowbite-svelte';
+
+	const languageEmojis: Record<AvailableLanguageTag, string> = {
+		en: '🇬🇧',
+		it: '🇮🇹'
+	};
+</script>
+
+<div>
+	<Button>{languageEmojis[languageTag()]}</Button>
+	<Dropdown>
+		{#each availableLanguageTags as tag}
+			<DropdownItem
+				on:click={() => {
+					setLanguageTag(tag);
+					console.log(languageTag());
+				}}
+			>
+				<p class="text-xl">
+					{languageEmojis[tag]}
+				</p>
+			</DropdownItem>
+		{/each}
+	</Dropdown>
+</div>
