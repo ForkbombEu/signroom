2e139caf755a90b7a4221f6f5653412d2800af86
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-31 11:47:57 +0100

    feat: configured i18n export

--- webapp/src/lib/i18n/index.ts
+++ webapp/src/lib/i18n/index.ts
@@ -1,4 +1,7 @@
 import { createI18n } from '@inlang/paraglide-js-adapter-sveltekit';
 import * as runtime from '$paraglide/runtime.js';
+import * as m from '$paraglide/messages';
 
 export const i18n = createI18n(runtime);
+
+export { m, runtime };
