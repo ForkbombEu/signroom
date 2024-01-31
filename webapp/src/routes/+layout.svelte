019dbc39172dd29171fae428ad83f1997befc920
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-31 11:14:00 +0100

    feat: configured paraglide for sveltekit

--- webapp/src/routes/+layout.svelte
+++ webapp/src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import '@fontsource-variable/inter';
 	import '../app.postcss';
+
+	import { ParaglideJS } from '@inlang/paraglide-js-adapter-sveltekit';
+	import { i18n } from '$lib/i18n';
 </script>
 
-<slot />
+<ParaglideJS {i18n}>
+	<slot />
+</ParaglideJS>
--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { paraglide } from '@inlang/paraglide-js-adapter-sveltekit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [
+		paraglide({
+			project: './project.inlang',
+			outdir: './src/paraglide'
+		}),
+		sveltekit()
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
