b8a6c03db8a12f080bd60e779259ca81121f2cdf
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2024-02-07 17:54:21 +0100

    feat: service qr code (#365)

---
+++ webapp/src/lib/qrcode/index.ts
@@ -0,0 +1,30 @@
+import type { ServicesRecord } from '$lib/pocketbase/types';
+import { zencode_exec } from 'zenroom';
+//@ts-ignore
+import { Slangroom } from '@slangroom/core';
+//@ts-ignore
+import { qrcode } from '@slangroom/qrcode';
+
+const scriptCreate = `Rule unknown ignore
+
+Given I send text 'text' and create qr code and output into 'qrcode'
+Given I have a 'string' named 'qrcode'
+Then print data
+`;
+const slangroom = new Slangroom(qrcode);
+
+export const generateQr = async (service: ServicesRecord) => {
+	const qr = await slangroom.execute(`
+Rule unknown ignore
+
+Given I send text 'text' and create qr code and output into 'qrcode'
+Given I have a 'string' named 'qrcode'
+Then print data
+`, {
+		data: {
+			text:JSON.stringify(service),
+		}
+	});
+    console.log(qr);
+    return qr
+};
