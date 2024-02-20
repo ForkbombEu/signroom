352c12eca4fa53fbe9b9411c7f298c299e3ea73c
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-20 15:43:13 +0100

    feat: update json builder package

--- webapp/src/routes/my/organizations/[id]/services/_partials/JSONSchemaInput.svelte
+++ webapp/src/routes/my/organizations/[id]/services/_partials/JSONSchemaInput.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { formFieldProxy, type SuperForm } from 'sveltekit-superforms/client';
-	import { FieldWrapper } from '$lib/forms';
-	import { JSONSchemaField, JSONSchemaBuilder } from 'json-schema-builder-svelte';
-	import { Alert, TabItem, Tabs } from 'flowbite-svelte';
+	import { JSONSchemaEditor } from 'json-schema-builder-svelte';
 	import type { UnwrapEffects } from 'sveltekit-superforms';
 
 	type T = $$Generic<AnyZodObject>;
@@ -12,24 +10,7 @@
 	export let superform: SuperForm<UnwrapEffects<T>, any>;
 
 	const { value } = formFieldProxy(superform, field);
-
-	const openEditor = $value === '';
-	const openPlain = $value !== '';
+	console.log($value);
 </script>
 
-<FieldWrapper {field} {label}>
-	<Tabs style="full">
-		<TabItem open={openPlain} title="Plain schema">
-			<Alert color="yellow" border class="!p-3 text-xs mb-4">
-				If the schema was not created with the Visual Editor, changing mode will alter some fields
-				<strong>with breaking changes!</strong>.
-			</Alert>
-			<JSONSchemaField bind:schema={$value} />
-		</TabItem>
-		<TabItem open={openEditor} title="Visual editor">
-			<div class="space-y-6">
-				<JSONSchemaBuilder bind:schema={$value} />
-			</div>
-		</TabItem>
-	</Tabs>
-</FieldWrapper>
+<JSONSchemaEditor {label} bind:schema={$value} mode="builder" returnType="string" />
