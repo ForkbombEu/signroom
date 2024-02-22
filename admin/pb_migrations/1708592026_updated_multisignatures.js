b6dcc19b42b35ad69b9ddd4edbce159b2526a7e0
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-22 10:21:45 +0100

    feat: create multisignatures collection

---
+++ admin/pb_migrations/1708592026_updated_multisignatures.js
@@ -0,0 +1,24 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("qdxfnllz52i1z1i")
+
+  collection.listRule = "owner.id = @request.auth.id || requests.recipient.id ?= @request.auth.id"
+  collection.viewRule = "owner.id = @request.auth.id || requests.recipient.id ?= @request.auth.id"
+  collection.createRule = "owner.id = @request.auth.id"
+  collection.updateRule = "owner.id = @request.auth.id"
+  collection.deleteRule = "owner.id = @request.auth.id"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("qdxfnllz52i1z1i")
+
+  collection.listRule = null
+  collection.viewRule = null
+  collection.createRule = null
+  collection.updateRule = null
+  collection.deleteRule = null
+
+  return dao.saveCollection(collection)
+})
