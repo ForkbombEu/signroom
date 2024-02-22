2913182c501e03c1fb165e9bbb98de282507021c
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-22 16:08:00 +0100

    feat: ui updates

--- webapp/src/lib/collectionManager/ui/collectionEmptyState.svelte
+++ webapp/src/lib/collectionManager/ui/collectionEmptyState.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
 	import { Heading, P } from 'flowbite-svelte';
 	import { CreateRecord } from './recordActions';
-	import { XCircle } from 'svelte-heros-v2';
+	import { Document } from 'svelte-heros-v2';
 	import type { IconComponent } from '$lib/utils/types';
 
 	export let title = 'No records here!';
 	export let description = 'Start by adding a record to this collection.';
-	export let icon: IconComponent = XCircle;
+	export let icon: IconComponent = Document;
 	export let hideCreateButton: boolean = false;
 </script>
 
 <div class="w-full p-10 text-center border-2 rounded-lg">
 	<div class="flex flex-col items-center justify-center space-y-6">
-		<div class="shrink-0 rounded-full flex items-center justify-center text-gray-400">
+		<div class="shrink-0 rounded-full flex items-center justify-center text-gray-200">
 			<svelte:component this={icon} size="160px" />
 		</div>
 		<div class="flex flex-col items-center justify-center">
-			<Heading tag="h4">{title}</Heading>
+			<Heading tag="h4" color="text-gray-400">{title}</Heading>
 			<P class="w-fit text-gray-400 font-medium">{description}</P>
 		</div>
 		{#if !hideCreateButton}
--- webapp/src/routes/my/multisignatures/+page.svelte
+++ webapp/src/routes/my/multisignatures/+page.svelte
@@ -10,29 +10,32 @@
 
 	import { Eye, Plus } from 'svelte-heros-v2';
 	import { Button, Heading } from 'flowbite-svelte';
+	import Card from '$lib/components/card.svelte';
 
 	//
 
 	const recordType = createTypeProp<MultisignaturesResponse>();
 </script>
 
-<CollectionManager {recordType} collection={Collections.Multisignatures} let:records>
-	<CollectionManagerHeader hideCreateButton>
-		<svelte:fragment slot="title">
-			<Heading tag="h3">Multisignatures</Heading>
-		</svelte:fragment>
-		<svelte:fragment slot="actions">
-			<Button color="alternative" href="/my/multisignatures/requests">
-				<Eye /><span class="ml-2">View requests</span>
-			</Button>
-			<Button href="/my/multisignatures/create">
-				<Plus /><span>Create a new multisignature</span>
-			</Button>
-		</svelte:fragment>
-	</CollectionManagerHeader>
-	<CollectionTable {records}>
-		<svelte:fragment slot="emptyState">
-			<CollectionEmptyState hideCreateButton />
-		</svelte:fragment>
-	</CollectionTable>
-</CollectionManager>
+<Card>
+	<CollectionManager {recordType} collection={Collections.Multisignatures} let:records>
+		<CollectionManagerHeader hideCreateButton>
+			<svelte:fragment slot="title">
+				<Heading tag="h4">Multisignatures</Heading>
+			</svelte:fragment>
+			<svelte:fragment slot="actions">
+				<Button color="alternative" href="/my/multisignatures/seals">
+					<Eye /><span class="ml-2">View seals</span>
+				</Button>
+				<Button href="/my/multisignatures/create">
+					<Plus /><span>Create a new multisignature</span>
+				</Button>
+			</svelte:fragment>
+		</CollectionManagerHeader>
+		<CollectionTable {records}>
+			<svelte:fragment slot="emptyState">
+				<CollectionEmptyState hideCreateButton />
+			</svelte:fragment>
+		</CollectionTable>
+	</CollectionManager>
+</Card>
