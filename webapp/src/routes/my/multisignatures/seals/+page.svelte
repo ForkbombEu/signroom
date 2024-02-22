fe77b9d67863e939e45bde199b39f5633cdca6d2
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-22 16:08:21 +0100

    feat: rename `requests` to `seals` page

---
+++ webapp/src/routes/my/multisignatures/seals/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { Collections, type MultisignatureSealsResponse } from '$lib/pocketbase/types';
+	import {
+		CollectionManager,
+		CollectionTable,
+		CollectionEmptyState,
+		CollectionManagerHeader
+	} from '$lib/collectionManager';
+	import { createTypeProp } from '$lib/utils/typeProp';
+
+	import { ArrowLeft } from 'svelte-heros-v2';
+	import { Button, Heading } from 'flowbite-svelte';
+	import Card from '$lib/components/card.svelte';
+
+	//
+
+	const recordType = createTypeProp<MultisignatureSealsResponse>();
+</script>
+
+<Card>
+	<CollectionManager {recordType} collection={Collections.MultisignatureSeals} let:records>
+		<CollectionManagerHeader hideCreateButton>
+			<svelte:fragment slot="title">
+				<Heading tag="h4">Multisignatures Seals</Heading>
+			</svelte:fragment>
+			<svelte:fragment slot="actions">
+				<Button color="alternative" href="/my/multisignatures">
+					<ArrowLeft /><span class="ml-2">Back to Multisignatures</span>
+				</Button>
+			</svelte:fragment>
+		</CollectionManagerHeader>
+
+		<CollectionTable {records}>
+			<svelte:fragment slot="emptyState">
+				<CollectionEmptyState
+					hideCreateButton
+					title="No pending seals to sign"
+					description="Pending seals will appear here."
+				/>
+			</svelte:fragment>
+		</CollectionTable>
+	</CollectionManager>
+</Card>
