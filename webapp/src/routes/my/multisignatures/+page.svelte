6c1887f3999444aab77073caf8f1a643557afa79
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-22 10:51:40 +0100

    feat: created multisignatures page

---
+++ webapp/src/routes/my/multisignatures/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { Collections, type MultisignaturesResponse } from '$lib/pocketbase/types';
+	import {
+		CollectionManager,
+		CollectionTable,
+		CollectionEmptyState,
+		CollectionManagerHeader
+	} from '$lib/collectionManager';
+	import { createTypeProp } from '$lib/utils/typeProp';
+
+	import { Plus } from 'svelte-heros-v2';
+	import { Button, Heading } from 'flowbite-svelte';
+
+	//
+
+	const recordType = createTypeProp<MultisignaturesResponse>();
+</script>
+
+<CollectionManager {recordType} collection={Collections.Multisignatures} let:records>
+	<CollectionManagerHeader hideCreateButton>
+		<svelte:fragment slot="title">
+			<Heading tag="h3">Multisignatures</Heading>
+		</svelte:fragment>
+		<svelte:fragment slot="actions">
+			<Button><Plus /><span>Create a new multisignature</span></Button>
+		</svelte:fragment>
+	</CollectionManagerHeader>
+	<CollectionTable {records}>
+		<svelte:fragment slot="emptyState">
+			<CollectionEmptyState hideCreateButton />
+		</svelte:fragment>
+	</CollectionTable>
+</CollectionManager>
