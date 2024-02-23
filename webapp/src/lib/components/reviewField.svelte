39d5d45c891c561b2b89cf89f1a217fd05e8380b
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-02-23 13:03:27 +0100

    feat(components): new reviewField

---
+++ webapp/src/lib/components/reviewField.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let label: string | undefined = undefined;
+</script>
+
+<div class="space-y-1">
+	{#if label}
+		<p class="text-sm">{label}</p>
+	{/if}
+
+	<div class="px-3 py-2 rounded-lg bg-gray-100 text-gray-500">
+		<slot />
+	</div>
+</div>
