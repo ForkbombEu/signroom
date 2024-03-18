0aed16f9de6d7d19ff09eae6f940a6b27f29a1c7
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-03-18 19:52:36 +0100

    feat(credential-templates): improved display

--- webapp/src/routes/[[lang]]/my/organizations/[id]/credential-templates/+page.svelte
+++ webapp/src/routes/[[lang]]/my/organizations/[id]/credential-templates/+page.svelte
@@ -7,15 +7,31 @@
 	import { Collections, type TemplatesResponse } from '$lib/pocketbase/types';
 	import { createFieldComponent } from '$lib/recordForm/fieldSchemaToInput.svelte';
 	import { createTypeProp } from '$lib/utils/typeProp';
-	import { Heading } from 'flowbite-svelte';
+	import { Heading, P, TableHeadCell } from 'flowbite-svelte';
 	import JSONSchemaInput from '../credential-issuances/_partials/JSONSchemaInput.svelte';
 	import Textarea from '$lib/forms/fields/textarea.svelte';
 	import { m } from '$lib/i18n';
+
+	import { objectSchemaValidator } from '$lib/jsonSchema/types';
+	import { objectSchemaToCredentialSubject } from '@api/downloadCredentialIssuer/utils';
+
+	//
 
 	export let data;
 	let { organization } = data;
 
 	const recordType = createTypeProp<TemplatesResponse>();
+
+	//
+
+	function getTemplateCredentialSubject(schema: any) {
+		try {
+			const objectSchema = objectSchemaValidator.parse(schema);
+			return objectSchemaToCredentialSubject(objectSchema);
+		} catch (e) {
+			return undefined;
+		}
+	}
 </script>
 
 <CollectionManager
@@ -36,14 +52,32 @@
 	let:records
 >
 	<CollectionManagerHeader>
-		<Heading slot="title" tag="h4">{m.Credential_templates()}</Heading>
+		<div slot="title" class="space-y-1">
+			<Heading tag="h4">{m.Credential_templates()}</Heading>
+		</div>
 	</CollectionManagerHeader>
 
 	<CollectionTable {records} fields={['name']} hideActions={['share', 'delete', 'select', 'edit']}>
+		<svelte:fragment slot="header">
+			<TableHeadCell>Properties</TableHeadCell>
+		</svelte:fragment>
+
 		<svelte:fragment let:record>
-			<div class="max-h-[200px] overflow-scroll bg-gray-100 p-3 rounded-lg">
-				<pre>{JSON.stringify(record.schema, null, 2)}</pre>
-			</div>
+			{@const credentialSubject = getTemplateCredentialSubject(record.schema)}
+			{#if credentialSubject}
+				<div class="max-h-[200px] overflow-scroll bg-gray-100 p-3 rounded-lg font-mono">
+					<ul class="list-disc list-inside">
+						{#each Object.entries(credentialSubject) as [propertyName, property]}
+							<li>
+								{propertyName}
+								{#if property.mandatory}
+									<span class="text-gray-300">[required]</span>
+								{/if}
+							</li>
+						{/each}
+					</ul>
+				</div>
+			{/if}
 		</svelte:fragment>
 	</CollectionTable>
 </CollectionManager>
