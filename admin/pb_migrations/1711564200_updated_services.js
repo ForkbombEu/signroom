038e1f88bac544cb9ee9998c916e1fd4eb3ee7dc
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-03-27 20:07:12 +0100

    feat(db): issuance flow: add authorization server fields

---
+++ admin/pb_migrations/1711564200_updated_services.js
@@ -0,0 +1,50 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("p8gn37nyhp1aif6")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "zsqrou6g",
+    "name": "external_verification_code",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "awp3ftsk",
+    "name": "external_verification_data",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("p8gn37nyhp1aif6")
+
+  // remove
+  collection.schema.removeField("zsqrou6g")
+
+  // remove
+  collection.schema.removeField("awp3ftsk")
+
+  return dao.saveCollection(collection)
+})
