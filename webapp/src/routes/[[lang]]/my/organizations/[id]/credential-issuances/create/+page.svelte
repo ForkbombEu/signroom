189ac2e7b7669fa6623423524487f78c08d24420
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-03-27 21:47:33 +0100

    feat(issuance form): sd-jwt default

--- webapp/src/routes/[[lang]]/my/organizations/[id]/credential-issuances/create/+page.svelte
+++ webapp/src/routes/[[lang]]/my/organizations/[id]/credential-issuances/create/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+	import { ServicesCredentialTypeOptions } from '$lib/pocketbase/types';
 	import ServiceForm from '../_partials/serviceForm.svelte';
 	export let data;
 </script>
 
-<ServiceForm organizationId={data.organization.id} />
+<ServiceForm
+	organizationId={data.organization.id}
+	initialData={{
+		credential_type: ServicesCredentialTypeOptions['sd-jwt']
+	}}
+/>
