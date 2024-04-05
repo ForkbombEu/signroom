4b2403c8bf0dc92fd094f8e4e0238c677925fb0d
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-04-05 07:43:43 +0200

    feat: updated components

---
+++ webapp/src/lib/components/icon.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { IconComponent } from '$lib/utils/types';
+	import clsx from 'clsx';
+
+	export let src: IconComponent;
+	export let size = 20;
+
+	export let ml = false;
+	export let mr = false;
+
+	let className = '';
+	export { className as class };
+
+	$: classes = clsx(className, {
+		'mr-2': mr,
+		'ml-2': ml
+	});
+</script>
+
+<svelte:component this={src} size={size.toString()} class={classes}></svelte:component>
--- webapp/src/lib/components/plainCard.svelte
+++ webapp/src/lib/components/plainCard.svelte
@@ -1,6 +1,11 @@
+<script lang="ts">
+	import Title from './plainCardTitle.svelte';
+	import Description from './plainCardDescription.svelte';
+</script>
+
 <div class="p-5 flex items-center justify-between border rounded-lg">
 	<div>
-		<slot />
+		<slot {Title} {Description} />
 	</div>
 	<div>
 		<slot name="right" />
