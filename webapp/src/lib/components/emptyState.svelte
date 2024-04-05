bd687eecc9bb4bcb7853dcb72c127669fc091d64
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-04-05 10:48:54 +0200

    feat(components): improved empty state

---
+++ webapp/src/lib/components/emptyState.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { FolderOpen } from 'svelte-heros-v2';
+	import type { IconComponent } from '$lib/utils/types';
+	import Icon from './icon.svelte';
+
+	export let title: string;
+	export let description: string | undefined = undefined;
+	export let icon: IconComponent = FolderOpen;
+</script>
+
+<div class="p-10 text-center border rounded-lg flex flex-col items-center justify-center">
+	<div class="text-gray-400">
+		<Icon src={icon} size={40} />
+	</div>
+
+	<slot name="title">
+		<p class="text-gray-400 font-semibold">
+			{title}
+		</p>
+	</slot>
+
+	<slot name="description">
+		{#if description}
+			<p class=" text-sm w-fit text-gray-400 font-medium">{description}</p>
+		{/if}
+	</slot>
+
+	<slot name="bottom" />
+</div>
