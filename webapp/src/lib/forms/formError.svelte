ae713443ba6dd859359b2443959ff83134135018
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-04-30 18:37:18 +0200

    feat: password reset (#568)
    * feat: improved forms module
    
    * feat: fix forgot password
    
    * feat: fix reset password page
    
    * fix: removed bad folder
    
    * feat: add migrations for reset password url
    
    * feat: add error message
    
    * feat(i18n): error

--- webapp/src/lib/forms/formError.svelte
+++ webapp/src/lib/forms/formError.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
 	import { Alert } from 'flowbite-svelte';
 	import { getFormContext } from './form.svelte';
+	import { m } from '$lib/i18n';
 
 	const { superform } = getFormContext();
-	const { message } = superform;
+	const { message, allErrors } = superform;
+
+	$: error = $allErrors.at(0);
 </script>
+
+{#if error}
+	<Alert color="red" dismissable>
+		<p class="font-bold">{m.Error()}</p>
+		{#if error.messages.length > 0}
+			<ul class="space-y-2 mt-1">
+				{#each error.messages as message}
+					<li class="leading-4">
+						{message}
+					</li>
+				{/each}
+			</ul>
+		{/if}
+	</Alert>
+{/if}
 
 {#if $message}
 	<Alert color="red" dismissable>
---
+++ webapp/src/routes/[[lang]]/(nru)/forgot-password/confirm/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Heading, P } from 'flowbite-svelte';
+	import { m } from '$lib/i18n';
+</script>
+
+<div class="space-y-4">
+	<Heading tag="h4">{m.Reset_email_sent_successfully()}</Heading>
+	<P>{m.Please_click_the_link_in_the_email_to_reset_your_password_()}</P>
+	<P>{m.You_can_now_close_this_page()}</P>
+</div>
