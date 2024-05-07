2b956c50179ce09e0bdd1e3d400f13bc6716869f
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-05-07 17:20:41 +0200

    feat: setup multisignature page

--- webapp/src/routes/[[lang]]/my/multisignatures/[multisignature_id]/+page.ts
+++ webapp/src/routes/[[lang]]/my/multisignatures/[multisignature_id]/+page.ts
@@ -1,12 +1,46 @@
 import { pb } from '$lib/pocketbase/index.js';
-import { Collections, type MultisignaturesResponse } from '$lib/pocketbase/types.js';
+import {
+	Collections,
+	type CoconutCredentialIssuersResponse,
+	type MultisignatureSealsResponse,
+	type MultisignaturesResponse,
+	type UsersResponse
+} from '$lib/pocketbase/types.js';
 
-export const load = async ({ params }) => {
+export const load = async ({ params, fetch }) => {
+	const ownerExpand = 'owner';
+	const issuerExpand = 'coconut_credential_issuer';
+
+	type Multisignatures = MultisignaturesResponse<
+		unknown,
+		{ [ownerExpand]: UsersResponse; [issuerExpand]: CoconutCredentialIssuersResponse }
+	>;
+
 	const multisignature = await pb
 		.collection(Collections.Multisignatures)
-		.getOne<MultisignaturesResponse>(params.multisignature_id);
+		.getOne<Multisignatures>(params.multisignature_id, {
+			expand: [ownerExpand, issuerExpand].join(', ')
+		});
+
+	//
+
+	type Seals = MultisignatureSealsResponse<
+		unknown,
+		{
+			[ownerExpand]: UsersResponse;
+		}
+	>;
+
+	const seals = await pb.collection(Collections.MultisignatureSeals).getFullList<Seals>({
+		filter: `multisignature.id = '${multisignature.id}'`,
+		fetch,
+		expand: ownerExpand
+	});
+
+	//
 
 	return {
-		multisignature
+		multisignature,
+		seals
 	};
 };
