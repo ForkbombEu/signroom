2da73afa05448bdd3415aeb47d3e9a3994e52d9c
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-05-07 13:54:22 +0200

    feat: improved multisig/review style

--- webapp/src/routes/[[lang]]/my/multisignatures/create/review/+page.svelte
+++ webapp/src/routes/[[lang]]/my/multisignatures/create/review/+page.svelte
@@ -4,18 +4,19 @@
 		multisignatureFormData,
 		createMultisignatureAndSeals
 	} from '../logic';
-	import { goto } from '$lib/i18n';
 	import { Form, createForm, SubmitButton, FormError } from '$lib/forms';
 	import { currentUser, pb } from '$lib/pocketbase';
 
 	import { assets } from '$app/paths';
-	import Card from '$lib/components/card.svelte';
 	import SideCard from '$lib/components/sideCard.svelte';
 	import SectionTitle from '$lib/components/sectionTitle.svelte';
 	import { Button } from 'flowbite-svelte';
 	import { ArrowRight, ArrowLeft } from 'svelte-heros-v2';
 	import { Collections, type UsersResponse } from '$lib/pocketbase/types';
 	import ReviewField from '$lib/components/reviewField.svelte';
+	import PageTop from '$lib/components/pageTop.svelte';
+	import PageContent from '$lib/components/pageContent.svelte';
+	import PageCard from '$lib/components/pageCard.svelte';
 
 	//
 
@@ -38,52 +39,51 @@
 	}
 </script>
 
-<Form {superform}>
-	<div class="flex items-start gap-8">
-		<div class="space-y-8 grow">
-			<div class="p-6 pb-0">
-				<SectionTitle
-					title="Review multisignature"
-					description="Review the content of the multisignature before confirming."
-				/>
-			</div>
+<PageTop>
+	<SectionTitle
+		title="Review multisignature"
+		description="Review the content of the multisignature before confirming."
+	/>
+</PageTop>
 
-			<Card class="p-6 space-y-6">
-				<SectionTitle tag="h5" title="Multisignature details" />
+<PageContent class="flex !space-y-0 items-start gap-8">
+	<PageCard class="p-6 space-y-6 grow">
+		<SectionTitle tag="h5" title="Multisignature details" />
 
-				<ReviewField label="Signature name">
-					<p>{$multisignatureFormData.name}</p>
-				</ReviewField>
+		<ReviewField label="Signature name">
+			<p>{$multisignatureFormData.name}</p>
+		</ReviewField>
 
-				<ReviewField label="Organizer">
-					{#await getOwner() then owner}
-						<p>{owner.username}</p>
-					{/await}
-				</ReviewField>
+		<ReviewField label="Organizer">
+			{#await getOwner() then owner}
+				<p>{owner.username}</p>
+			{/await}
+		</ReviewField>
 
-				<ReviewField label="Participants">
-					{#await getParticipants() then participants}
-						{#each participants as p}
-							<p>{p.username}</p>
-						{/each}
-					{/await}
-				</ReviewField>
+		<ReviewField label="Participants">
+			{#await getParticipants() then participants}
+				<p>(You)</p>
+				{#each participants as p}
+					<p>{p.username}</p>
+				{/each}
+			{/await}
+		</ReviewField>
 
-				<ReviewField label="Content">
-					{$multisignatureFormData.contentJSON}
-				</ReviewField>
+		<ReviewField label="Content">
+			{$multisignatureFormData.contentJSON}
+		</ReviewField>
 
-				<ReviewField label="Deadline for signature">
-					{$multisignatureFormData.sealExpirationDate}
-				</ReviewField>
-			</Card>
-		</div>
+		<ReviewField label="Deadline for signature">
+			{$multisignatureFormData.sealExpirationDate}
+		</ReviewField>
+	</PageCard>
 
-		<SideCard title="Review and sign" image={`${assets}/multisignatures/multisignature-step-3.svg`}>
-			<svelte:fragment slot="top">
-				<SectionTitle title="Step 3 of 3" tag="h6" />
-			</svelte:fragment>
-			<svelte:fragment slot="bottom">
+	<SideCard title="Review and sign" image={`${assets}/multisignatures/multisignature-step-3.svg`}>
+		<svelte:fragment slot="top">
+			<SectionTitle title="Step 3 of 3" tag="h6" />
+		</svelte:fragment>
+		<svelte:fragment slot="bottom">
+			<Form {superform}>
 				<FormError />
 				<div class="flex justify-stretch gap-2">
 					<Button
@@ -93,11 +93,11 @@
 					>
 						<ArrowLeft />
 					</Button>
-					<SubmitButton class="px-4">
+					<SubmitButton>
 						<span class="mr-2 text-nowrap">Confirm and Sign</span><ArrowRight />
 					</SubmitButton>
 				</div>
-			</svelte:fragment>
-		</SideCard>
-	</div>
-</Form>
+			</Form>
+		</svelte:fragment>
+	</SideCard>
+</PageContent>
