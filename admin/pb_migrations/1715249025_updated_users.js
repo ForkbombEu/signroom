f23ed0f75d5ccc97567f989f2a09a8bfada5dca3
puria <puria@dyne.org>
2024-05-10 15:27:58 +0700

    cruft up

---
+++ admin/pb_migrations/1715249025_updated_users.js
@@ -0,0 +1,126 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove
+  collection.schema.removeField("nf3jan6h")
+
+  // remove
+  collection.schema.removeField("zjvzw9wa")
+
+  // remove
+  collection.schema.removeField("yg9yz2sc")
+
+  // remove
+  collection.schema.removeField("ouslgk2m")
+
+  // remove
+  collection.schema.removeField("vqu3zutd")
+
+  // remove
+  collection.schema.removeField("nkp4qhd9")
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "nf3jan6h",
+    "name": "bitcoin_public_key",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "zjvzw9wa",
+    "name": "ecdh_public_key",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "yg9yz2sc",
+    "name": "eddsa_public_key",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ouslgk2m",
+    "name": "ethereum_address",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "vqu3zutd",
+    "name": "reflow_public_key",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "nkp4qhd9",
+    "name": "es256_public_key",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
