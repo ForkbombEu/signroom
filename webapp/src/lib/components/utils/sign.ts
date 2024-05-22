2885b5ba90e7af5df445d79c7fafd66a73942a38
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-05-22 15:56:38 +0200

    refactor: signatures page

--- webapp/src/lib/components/utils/sign.ts
+++ webapp/src/lib/components/utils/sign.ts
@@ -2,13 +2,19 @@ import hexDerEs256Signature from '../../../../zenflows-crypto/src/hex_der_es256_
 import EdDSASignature from '../../../../zenflows-crypto/src/eddsa_signature.zen?raw';
 import { zencode_exec } from 'zenroom';
 import forge from 'node-forge';
+import { pb } from '$lib/pocketbase';
+import type { CertificatesResponse, SignaturesResponse } from '$lib/pocketbase/types';
+
+//
 
 async function zencodeExec(contract: string, data: string) {
 	const { result } = await zencode_exec(contract, { data: data });
 	return JSON.parse(result);
 }
 
-export async function signData(algorithmName: string, sk: string, data: string): Promise<string> {
+//
+
+async function signData(algorithmName: string, sk: string, data: string): Promise<string> {
 	switch (algorithmName) {
 		case 'ECDSA':
 			const { der_signature } = await zencodeExec(
@@ -51,5 +57,81 @@ export async function signData(algorithmName: string, sk: string, data: string):
 			return btoa(pssPrivateKey.sign(md, pss));
 		default:
 			throw algorithmName + '  not yet implemented';
+	}
+}
+
+//
+
+export async function signFile(record: SignaturesResponse) {
+	try {
+		const algo = record.type;
+		const url = pb.files.getUrl(record, record.file);
+		//get the file to sign
+		const res = await fetch(url);
+		const blob = await res.blob();
+
+		let fb64: string = await new Promise((resolve, reject) => {
+			const reader = new FileReader();
+			reader.readAsDataURL(blob);
+			reader.onerror = (e) => {
+				reject(e);
+			};
+			reader.onload = () => {
+				resolve((reader.result as string).split(',')[1]);
+			};
+		});
+
+		console.log(fb64);
+
+		const cert = await pb
+			.collection('certificates')
+			.getOne<CertificatesResponse>(record.certificate);
+		const name = cert.name;
+		const certPem = cert.value;
+		const signatureAlgorithmName = cert.algorithm;
+		// current timestamp
+		const ts_now = Date.now();
+		const sk = JSON.parse(localStorage.getItem('certificateKey') || '{}');
+		if (sk[name] == null) throw 'Empty secret key';
+		const secretKey = sk[name].zenroomValue || sk[name].value;
+
+		//2. get data to sign
+		const toSign = await fetch('/api/getDataToSign', {
+			method: 'POST',
+			body: JSON.stringify({ certPem, ts_now, algo, doc: fb64, signatureAlgorithmName }),
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			}
+		});
+		const toBeSigned = await toSign.json();
+
+		//3. sign digest of data
+		const signedDigest = await signData(signatureAlgorithmName, secretKey, toBeSigned.bytes);
+
+		//4. sign document (insert signature)
+		const signed = await fetch('/api/signDocument', {
+			method: 'POST',
+			body: JSON.stringify({
+				certPem,
+				ts_now,
+				algo,
+				doc: fb64,
+				signatureAlgorithmName,
+				signedDigest
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			}
+		});
+		const signedDocument = await signed.json();
+
+		//5. save signed document to db
+		const formData = new FormData();
+		formData.append('signed_file', JSON.stringify(signedDocument));
+		await pb.collection('signatures').update(record.id, formData);
+	} catch (e) {
+		throw e;
 	}
 }
--- webapp/src/routes/[[lang]]/my/certificates/+page.svelte
+++ webapp/src/routes/[[lang]]/my/certificates/+page.svelte
@@ -34,6 +34,8 @@
 		addCertifcateAndKey,
 		addAutosingedCertificateAndKey
 	} from './logic';
+	import PageContent from '$lib/components/pageContent.svelte';
+	import PageCard from '$lib/components/pageCard.svelte';
 
 	const schema = z.object({
 		name: z.string(),
@@ -135,126 +137,135 @@
 	);
 </script>
 
-<div class="p-4 space-y-4 border-slate-200 rounded-lg">
-	<CollectionManager collection={Collections.Certificates} {recordType} let:records>
-		<CollectionManagerHeader hideCreateButton>
-			<svelte:fragment slot="title">
-				<Heading tag="h4">My Certificates</Heading>
-			</svelte:fragment>
-			<svelte:fragment slot="actions">
-				<Button on:click={() => (showModal = true)}>
-					<Plus />
-					<span class="ml-2"> Add a Key-Certificate Pair </span>
-				</Button>
-			</svelte:fragment>
-		</CollectionManagerHeader>
-
-		<CollectionTable
-			{records}
-			fields={['name', 'algorithm']}
-			hideActions={['select', 'edit', 'share', 'delete']}
-		>
-			<svelte:fragment slot="emptyState">
-				<CollectionEmptyState
-					title="No certificate here"
-					description="Start by creating an autosigned certificate"
-					hideCreateButton
-				>
-					<svelte:fragment slot="bottom">
-						<Button color="alternative" on:click={() => (showAutosignedModal = true)}>
+<PageContent>
+	<PageCard>
+		<div class="p-4 space-y-4 border-slate-200 rounded-lg">
+			<CollectionManager
+				collection={Collections.Certificates}
+				{recordType}
+				let:records
+				hideEmptyState
+			>
+				<CollectionManagerHeader hideCreateButton>
+					<svelte:fragment slot="title">
+						<Heading tag="h4">My Certificates</Heading>
+					</svelte:fragment>
+					<svelte:fragment slot="actions">
+						<Button on:click={() => (showModal = true)}>
 							<Plus />
-							<span class="ml-2"> Generate an autosigned certificate </span>
+							<span class="ml-2"> Add a Key-Certificate Pair </span>
 						</Button>
 					</svelte:fragment>
-				</CollectionEmptyState>
-			</svelte:fragment>
+				</CollectionManagerHeader>
 
-			<svelte:fragment slot="header">
-				<TableHeadCell>Key status</TableHeadCell>
-			</svelte:fragment>
-			<svelte:fragment slot="row" let:record>
-				{@const keyExists = checkCertificateKeyInLocalStorage(record)}
-				<TableBodyCell>
-					{#if keyExists}
-						<Badge color="green">Available</Badge>
-					{:else}
-						<div class="flex items-center gap-2">
-							<Badge color="red">Missing</Badge>
-							<Button
-								size="xs"
-								color="alternative"
-								on:click={() => {
-									keyUploadCertificate = record;
-									keyUploadModal.on();
-								}}
-							>
-								<ArrowUpTray size="16"></ArrowUpTray>
-								<span class="ml-1.5">Load key</span>
-							</Button>
-						</div>
-					{/if}
-				</TableBodyCell>
-			</svelte:fragment>
+				<CollectionTable
+					{records}
+					fields={['name', 'algorithm']}
+					hideActions={['select', 'edit', 'share', 'delete']}
+				>
+					<svelte:fragment slot="emptyState">
+						<CollectionEmptyState
+							title="No certificate here"
+							description="Start by creating an autosigned certificate"
+							hideCreateButton
+						>
+							<svelte:fragment slot="bottom">
+								<Button color="alternative" on:click={() => (showAutosignedModal = true)}>
+									<Plus />
+									<span class="ml-2"> Generate an autosigned certificate </span>
+								</Button>
+							</svelte:fragment>
+						</CollectionEmptyState>
+					</svelte:fragment>
 
-			<svelte:fragment slot="actions" let:record>
-				<DeleteRecord
-					{record}
-					on:delete={(e) => {
-						onCertificateDelete(e.detail.record);
-					}}
-				/>
-			</svelte:fragment>
-		</CollectionTable>
-	</CollectionManager>
-</div>
+					<svelte:fragment slot="header">
+						<TableHeadCell>Key status</TableHeadCell>
+					</svelte:fragment>
+					<svelte:fragment slot="row" let:record>
+						{@const keyExists = checkCertificateKeyInLocalStorage(record)}
+						<TableBodyCell>
+							{#if keyExists}
+								<Badge color="green">Available</Badge>
+							{:else}
+								<div class="flex items-center gap-2">
+									<Badge color="red">Missing</Badge>
+									<Button
+										size="xs"
+										color="alternative"
+										on:click={() => {
+											keyUploadCertificate = record;
+											keyUploadModal.on();
+										}}
+									>
+										<ArrowUpTray size="16"></ArrowUpTray>
+										<span class="ml-1.5">Load key</span>
+									</Button>
+								</div>
+							{/if}
+						</TableBodyCell>
+					</svelte:fragment>
 
-<PortalWrapper>
-	<Modal bind:open={showModal} size="md" title="Key and certificate" placement="center">
-		<Form {superform}>
-			<Input
-				{superform}
-				field="name"
-				options={{ id: 'name', label: 'Insert your certificate name' }}
-			/>
-			<FileInput
-				{superform}
-				field="certificate"
-				options={{ id: 'certificate', label: 'Select your certificate' }}
-			/>
-			<FileInput {superform} field="key" options={{ id: 'key', label: 'Select your key' }} />
-			<FormError />
-			<dir class="flex justify-end">
-				<SubmitButton>Submit certificate and key</SubmitButton>
-			</dir>
-		</Form>
-	</Modal>
+					<svelte:fragment slot="actions" let:record>
+						<DeleteRecord
+							{record}
+							on:delete={(e) => {
+								onCertificateDelete(e.detail.record);
+							}}
+						/>
+					</svelte:fragment>
+				</CollectionTable>
+			</CollectionManager>
+		</div>
 
-	<Modal bind:open={$keyUploadModal} size="md" title="Load key" placement="center">
-		<Form superform={keyUploadForm}>
-			<FileInput superform={keyUploadForm} field="key" options={{ label: 'Select your key' }} />
-			<FormError />
-			<dir class="flex justify-end">
-				<SubmitButton>Submit key</SubmitButton>
-			</dir>
-		</Form>
-	</Modal>
+		<PortalWrapper>
+			<Modal bind:open={showModal} size="md" title="Key and certificate" placement="center">
+				<Form {superform}>
+					<Input
+						{superform}
+						field="name"
+						options={{ id: 'name', label: 'Insert your certificate name' }}
+					/>
+					<FileInput
+						{superform}
+						field="certificate"
+						options={{ id: 'certificate', label: 'Select your certificate' }}
+					/>
+					<FileInput {superform} field="key" options={{ id: 'key', label: 'Select your key' }} />
+					<FormError />
+					<dir class="flex justify-end">
+						<SubmitButton>Submit certificate and key</SubmitButton>
+					</dir>
+				</Form>
+			</Modal>
 
-	<Modal
-		bind:open={showAutosignedModal}
-		size="md"
-		title="Autosigned certificate"
-		placement="center"
-	>
-		<Form superform={autosignedCertForm}>
-			<Input
-				superform={autosignedCertForm}
-				field="name"
-				options={{ id: 'name', label: 'Insert the  certificate name' }}
-			/>
-			<FormError />
-			<dir class="flex justify-end">
-				<SubmitButton>Submit name</SubmitButton>
-			</dir>
-		</Form>
-	</Modal>
-</PortalWrapper>
+			<Modal bind:open={$keyUploadModal} size="md" title="Load key" placement="center">
+				<Form superform={keyUploadForm}>
+					<FileInput superform={keyUploadForm} field="key" options={{ label: 'Select your key' }} />
+					<FormError />
+					<dir class="flex justify-end">
+						<SubmitButton>Submit key</SubmitButton>
+					</dir>
+				</Form>
+			</Modal>
+
+			<Modal
+				bind:open={showAutosignedModal}
+				size="md"
+				title="Autosigned certificate"
+				placement="center"
+			>
+				<Form superform={autosignedCertForm}>
+					<Input
+						superform={autosignedCertForm}
+						field="name"
+						options={{ id: 'name', label: 'Insert the  certificate name' }}
+					/>
+					<FormError />
+					<dir class="flex justify-end">
+						<SubmitButton>Submit name</SubmitButton>
+					</dir>
+				</Form>
+			</Modal>
+		</PortalWrapper>
+	</PageCard>
+</PageContent>
