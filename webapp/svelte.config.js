3720ce0bcb02912575e8f14687b0583839d478c9
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-05-28 12:00:51 +0200

    chore: update vite and svelte config

--- webapp/svelte.config.js
+++ webapp/svelte.config.js
@@ -1,17 +1,11 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [
-		vitePreprocess(),
-		preprocess({
-			postcss: true
-		})
-	],
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter(),
---
+++ webapp/vite.config.ts
@@ -0,0 +1,10 @@
+import { paraglide } from '@inlang/paraglide-sveltekit/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [paraglide({ project: './project.inlang', outdir: './src/paraglide' }), sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
