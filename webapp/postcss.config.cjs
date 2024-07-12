aaf8c30b3a5a00365a4a90645770a7565d82c8ad
matteo-cristino <cristino.matteo97@gmail.com>
2024-07-12 12:32:44 +0200

    chore: add license header to last files in webapp folder

--- webapp/postcss.config.cjs
+++ webapp/postcss.config.cjs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 The Forkbomb Company
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 
--- webapp/tailwind.config.cjs
+++ webapp/tailwind.config.cjs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 The Forkbomb Company
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 const config = {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
