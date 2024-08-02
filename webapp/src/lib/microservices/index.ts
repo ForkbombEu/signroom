18b0b0be4040b7fb1a6bdcbf8be8b5bb09ac96de
Matteo Cristino <102997993+matteo-cristino@users.noreply.github.com>
2024-08-02 11:45:41 +0200

    ci: add reuse test (#734)
    * chore: add missing license headers
    
    * ci: add reuse test
    
    * refactor(reuse): adopt the new version of the metadata file
    
    * chore: add title to job names
    
    * chore: prevent duplicate builds
    
    * chore: add header annotation licensing to new file
    
    ---------
    
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>

--- webapp/src/lib/microservices/index.ts
+++ webapp/src/lib/microservices/index.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 The Forkbomb Company
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 export function getRandomMicroservicePort() {
 	const MIN_PORT = 33000;
 	const MAX_PORT = 65000;
--- webapp/src/routes/api/download-microservices/config.ts
+++ webapp/src/routes/api/download-microservices/config.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 The Forkbomb Company
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 export const config = {
 	folder_names: {
 		custom_code: 'custom_code',
