66f75c4724a28a830d77cd28ecce3701a7765b65
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-08-05 11:58:31 +0200

    fix: remove port limit

--- webapp/src/lib/microservices/index.ts
+++ webapp/src/lib/microservices/index.ts
@@ -4,6 +4,6 @@
 
 export function getRandomMicroservicePort() {
 	const MIN_PORT = 33000;
-	const MAX_PORT = 65000;
+	const MAX_PORT = 34000;
 	return MIN_PORT + Math.floor(Math.random() * (MAX_PORT - MIN_PORT));
 }
