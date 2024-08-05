c8fa5928093ba66d87d0fcb7ee87428f34de08ae
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-08-05 10:47:08 +0200

    fix: typescript

--- webapp/src/routes/[[lang]]/my/organizations/[id]/credential-issuances/[issuance_id]/+page.svelte
+++ webapp/src/routes/[[lang]]/my/organizations/[id]/credential-issuances/[issuance_id]/+page.svelte
@@ -5,7 +5,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
 <script lang="ts">
-	import { Button, Heading, Spinner } from 'flowbite-svelte';
+	import { Button, Heading } from 'flowbite-svelte';
 	import { ArrowTopRightOnSquare, Pencil, QuestionMarkCircle } from 'svelte-heros-v2';
 	import { generateQr } from '$lib/qrcode';
 	import { m } from '$lib/i18n';
