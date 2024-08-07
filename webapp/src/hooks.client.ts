90236df91a6fa811033673f3110fef8a8386161a
Puria Nafisi Azizi <puria@dyne.org>
2024-08-07 12:51:37 +0200

    chore: update sentry (#735)

--- webapp/src/hooks.client.ts
+++ webapp/src/hooks.client.ts
@@ -3,14 +3,17 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import * as Sentry from '@sentry/sveltekit';
-import { currentUser, pb, type AuthStoreModel } from '$lib/pocketbase';
+import { currentUser, pb } from '$lib/pocketbase';
+import type { AuthStoreModel } from '$lib/pocketbase';
 
 // If you don't want to use Session Replay, remove the `Replay` integration,
 // `replaysSessionSampleRate` and `replaysOnErrorSampleRate` options.
 Sentry.init({
 	dsn: 'https://f1256ba86cba40e094cf988b4ce32aae@o822807.ingest.sentry.io/4505521418338304',
-	tracesSampleRate: 1,
-	replaysSessionSampleRate: 0.1,
+	integrations: [
+		Sentry.replayIntegration(),
+	],
+	replaysSessionSampleRate: 1,
 	replaysOnErrorSampleRate: 1
 });
 
