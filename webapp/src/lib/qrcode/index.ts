20b3dfc9591cfb3b6131a1c9d9223259da2221e4
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2024-09-17 16:48:22 +0200

    feat: credential-issuance qr code contains intent-url (#795)
    * feat: credential-issuance qr code contains intent-url
    
    * fix: remove qrcode intent-url protocol variable
    
    * fix: protocol for wallet intent openid4vp
    
    ---------
    
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>

--- webapp/src/lib/qrcode/index.ts
+++ webapp/src/lib/qrcode/index.ts
@@ -4,6 +4,19 @@
 
 import qrcode from 'qrcode-generator';
 
+type CredentialConfig = {
+	credential_configuration_ids: string[];
+	credential_issuer: string;
+};
+
+export function createIntentUrl(data: CredentialConfig): string {
+	const credentialConfigurationIds = encodeURIComponent(
+		data.credential_configuration_ids.join(',')
+	);
+	const credentialIssuer = encodeURIComponent(data.credential_issuer);
+	return `openid4vp://?credential_configuration_ids=${credentialConfigurationIds}&credential_issuer=${credentialIssuer}`;
+}
+
 export function generateQr(text: string, cellSize = 20) {
 	const qr = qrcode(0, 'L');
 	qr.addData(text);
