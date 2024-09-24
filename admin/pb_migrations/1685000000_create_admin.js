c627ade4044eb04c77c32a9b7876bc6ddbc76ae4
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-09-24 17:02:05 +0200

    fix migrations

--- admin/pb_migrations/1685000000_create_admin.js
+++ admin/pb_migrations/1685000000_create_admin.js
@@ -1,13 +1,23 @@
+/// <reference path="../pb_data/types.d.ts" />
+
+const ADMIN_EMAIL = "admin@example.org";
+
 migrate(
     (db) => {
-        const admin = new Admin();
-        admin.email = "admin@example.org";
-        admin.setPassword("adminadmin");
-        return Dao(db).saveAdmin(admin);
+        const dao = new Dao(db);
+
+        try {
+            dao.findAdminByEmail(ADMIN_EMAIL);
+        } catch {
+            const admin = new Admin();
+            admin.email = ADMIN_EMAIL;
+            admin.setPassword("adminadmin");
+            return dao.saveAdmin(admin);
+        }
     },
     (db) => {
         const dao = new Dao(db);
-        const admin = dao.findAdminByEmail("admin@example.org");
+        const admin = dao.findAdminByEmail(ADMIN_EMAIL);
         return dao.deleteAdmin(admin);
     }
 );
