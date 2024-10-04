a102344afac1716c2bbd47822509453ca70c7c4c
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-10-04 13:04:17 +0200

    feat: updated i18n utils

--- webapp/src/routes/tests/record-form/+page.svelte
+++ webapp/src/routes/tests/record-form/+page.svelte
@@ -19,8 +19,6 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 	import { createFieldComponent } from '$lib/recordForm/fieldSchemaToInput.svelte';
 	import { createTypeProp } from '$lib/utils/typeProp';
 
-	export let data;
-
 	const recordType = createTypeProp<
 		CrudExampleResponse<{
 			relation: FeaturesResponse[];
