972296c6c8152525d8badf8d91623d630d923dd4
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-10-09 11:58:53 +0200

    reorganized emails

--- webapp/static/emails/_emailData.d.ts
+++ webapp/static/emails/_emailData.d.ts
@@ -1,4 +1,120 @@
 type EmailData = {
+    "_account-deactivation": [
+        "username",
+        "reactivateLink",
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
+    "_flow-update_advanced": [
+        "username",
+        "FlowName",
+        "OrganizationName",
+        "Editor",
+        "IsPublic",
+        "API",
+        "DashboardLink",
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
+    "_flow-update_cryptography": [
+        "username",
+        "FlowName",
+        "OrganizationName",
+        "Editor",
+        "Cryptography",
+        "Expiration",
+        "DashboardLink",
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
+    "_flow-update_microservices": [
+        "UserName",
+        "FlowName",
+        "OrganizationName",
+        "Editor",
+        "AuthServer",
+        "CredentialIssuer",
+        "DashboardLink",
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
+    "_flow-update_name": [
+        "UserName",
+        "FlowName",
+        "OrganizationName",
+        "Editor",
+        "FlowName",
+        "DashboardLink",
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
+    "_flow-update_templates": [
+        "UserName",
+        "FlowName",
+        "OrganizationName",
+        "Editor",
+        "AuthTemplateName",
+        "CredentialTemplateName",
+        "DashboardLink",
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
+    "_microservice-warning": [
+        "UserName",
+        "MicroserviceName",
+        "OrganizationName",
+        "ErrorWarningDetails",
+        "ResolutionSteps",
+        "DashboardLink",
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
+    "_update-subscription": [
+        "UserName",
+        "SubscriptionPlan",
+        "RenewalDate",
+        "PaymentStatus",
+        "DashboardLink",
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
+    "_user-invitation-reminder": [
+        "OrganizationName",
+        "UserName",
+        "InvitationLink",
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
+    "_verification-flow-update_microservices": [
+        "UserName",
+        "FlowName",
+        "OrganizationName",
+        "Editor",
+        "RelyingParty",
+        "DashboardLink",
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
+    "_verification-flow-update_name": [
+        "UserName",
+        "FlowName",
+        "OrganizationName",
+        "Editor",
+        "VerificationFlowName",
+        "DashboardLink",
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
+    "_verification-flow-update_templates": [
+        "username",
+        "FlowName",
+        "OrganizationName",
+        "Editor",
+        "VerificationTemplateName",
+        "DashboardLink",
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
     "account-deactivation": [
         "username",
         "reactivateLink",
@@ -6,6 +122,29 @@ type EmailData = {
         "unsubscribeLink"
     ],
     "example": [
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
+    "flow-new": [
+        "FlowName",
+        "UserName",
+        "OrganizationName",
+        "Editor",
+        "FlowName",
+        "FlowImage",
+        "FlowName",
+        "FlowDescription",
+        "cryptography",
+        "expiration",
+        "AuthTemplateName",
+        "CredentialTemplateDescription",
+        "CredentialTemplateName",
+        "CredentialTemplateDescription",
+        "AuthServer",
+        "CredentialIssuer",
+        "IsPublic",
+        "API",
+        "DashboardLink",
         "viewInBrowserLink",
         "unsubscribeLink"
     ],
@@ -80,6 +219,18 @@ type EmailData = {
         "UserName",
         "Editor",
         "OrganizationName",
+        "DashboardLink",
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
+    "microservice-new": [
+        "MicroserviceName",
+        "UserName",
+        "MicroserviceName",
+        "OrganizationName",
+        "Editor",
+        "Functionality",
+        "ConfigurationOptions",
         "DashboardLink",
         "viewInBrowserLink",
         "unsubscribeLink"
@@ -213,6 +364,17 @@ type EmailData = {
         "OrganizationName",
         "InvitationLink",
         "InvitationLink",
+        "viewInBrowserLink",
+        "unsubscribeLink"
+    ],
+    "verification-flow-new": [
+        "VerificationFlowName",
+        "UserName",
+        "OrganizationName",
+        "Editor",
+        "VerificationFlowName",
+        "VerificationFlowDescription",
+        "DashboardLink",
         "viewInBrowserLink",
         "unsubscribeLink"
     ],
