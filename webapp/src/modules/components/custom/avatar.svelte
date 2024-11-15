87f3593d28da84d6dc0faac6d76ea05b7e0d4926
puria <puria@users.noreply.github.com>
2024-11-15 02:54:42 +0000

    chore: accept new Cruft update

---
+++ webapp/src/modules/components/custom/avatar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { m } from '@/i18n';
+	import * as Avatar from '@/components/ui/avatar';
+	import type { AvatarProps } from 'bits-ui';
+
+	type $$Props = AvatarProps & { src?: string; alt?: string; fallback?: string };
+	$: props = $$props as $$Props;
+</script>
+
+<Avatar.Root {...$$props}>
+	{#if props.src}
+		<Avatar.Image src={props.src} alt={props.alt ?? m.Avatar()} />
+	{/if}
+	{#if props.fallback}
+		<Avatar.Fallback>{props.fallback}</Avatar.Fallback>
+	{/if}
+</Avatar.Root>
