ab0c810970d3f72e5285f12785540a3fc74ea5e4
puria <puria@users.noreply.github.com>
2024-11-16 02:51:51 +0000

    chore: accept new Cruft update

---
+++ webapp/src/modules/collections-components/form/index.ts
@@ -0,0 +1,4 @@
+import CollectionForm from './collectionForm.svelte';
+import type { CollectionFormOptions } from './formOptions';
+
+export { CollectionForm, type CollectionFormOptions };
---
+++ webapp/src/modules/components/layout/pageContent.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import clsx from 'clsx';
+
+	//
+
+	export let layout: 'vertical' | 'horizontal' = 'vertical';
+
+	let className = '';
+	export { className as class };
+
+	//
+
+	$: classes = clsx('px-10 py-8', className, {
+		'flex items-start gap-8': layout == 'horizontal',
+		'space-y-8': layout == 'vertical'
+	});
+</script>
+
+<div class={classes}>
+	<slot />
+</div>
